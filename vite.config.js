100822f094a715ec58076311d475803a9ae1304e
“Hasindu” <hasindukl@gmail.comgit config --global user.email hasinduexpernetic.com>
2025-03-03 11:31:11 +0530

    Refac: code refactor

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss()],
-  baseUrl: '/Viyu_Web',
+  baseUrl: '/Viyu_Web/',
 })
